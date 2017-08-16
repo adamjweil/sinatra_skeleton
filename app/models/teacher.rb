756f3ecc66093730a0df9c2bdc63b7cabc652df6
Adam Weil <adamjweil@gmail.com>
2017-08-16 15:48:15 -0400

    note and teacher model

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,11 +1,10 @@
-class User < ActiveRecord::Base
+class Teacher < ActiveRecord::Base
   # Remember to create a migration!
+  has_many :users
   has_many :notes
-  has_many :teachers
 
   validates :first_name, :last_name, :username, :email, :password_digest, presence: true
   validates :email, uniqueness: true
 
   has_secure_password
-
 end
--- db/migrate/20170816154118_create_teachers.rb
+++ db/migrate/20170816154118_create_teachers.rb
@@ -1,4 +1,13 @@
 class CreateTeachers < ActiveRecord::Migration
   def change
+    create_table :teachers do |t|
+      t.string :first_name, NULL: false
+      t.string :last_name, NULL: false
+      t.string :username, NULL: false
+      t.string :email, NULL: false
+      t.string :password_digest, NULL: false
+
+      t.timestamps(NULL: false)
+    end
   end
 end
