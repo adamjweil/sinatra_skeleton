1596534a69edda0f7e78a221c53872ce8473e943
Adam Weil <adamjweil@gmail.com>
2017-09-14 14:32:55 -0400

    updateding skeliton

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,36 +5,3 @@ end
 get '/index' do
   erb :index
 end
-
-# register
-get '/users/new' do
-  erb :'users/new'
-end
-
-post '/users' do
-  create_user
-end
-
-get '/users/show' do
-  @current_user
-  erb :'users/show'
-end
-
-# login
-get '/sessions/new' do
-  erb :'sessions/new'
-end
-post '/sessions' do
-  login
-end
-
-
-delete '/logout' do
-  session.delete(:user_id)
-  redirect '/'
-end
-get '/logout' do
-  # session[:user_id] = nil
-  session.delete(:user_id)
-  redirect '/'
-end
--- app/models/resource.rb
+++
@@ -1,6 +0,0 @@
-class Resource < ActiveRecord::Base
-  # Remember to create a migration!
-  validates :title, :description, :user_id, presence: true
-  belongs_to :user
-
-end
